9bd54320861b91920125ecfbfede1d6aacf3d9b4
vllutan <43119331+vllutan@users.noreply.github.com>
2022-04-26 02:18:36 +0300

    Add files via upload

---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using CLab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+
+    public partial class MainWindow : Window
+    {
+        ViewData vd = new ViewData();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = vd;
+        }
+
+        private void MenuFile_New(object sender, RoutedEventArgs e)
+        {
+            if (vd.Change == true)
+            {
+                MessageBoxResult res = MessageBox.Show("Data will be lost. Save Data?", "", MessageBoxButton.YesNoCancel);
+                if (res == MessageBoxResult.Yes)
+                {
+                    MenuFile_Save(sender, e);
+                }
+                else if (res == MessageBoxResult.Cancel) return;
+            }
+
+            vd.vmb.time.Clear();
+            vd.vmb.accuracy.Clear();
+            vd = new ViewData();
+            DataContext = vd;
+        }
+
+        private void MenuFile_Open(object sender, RoutedEventArgs e)
+        {
+            if (vd.Change == true)
+            {
+                MessageBoxResult res = MessageBox.Show("Data will be lost. Save Data?", "", MessageBoxButton.YesNoCancel);
+                if (res == MessageBoxResult.Yes)
+                {
+                    MenuFile_Save(sender, e);
+                }
+                else if (res == MessageBoxResult.Cancel) return;
+            }
+
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            if(dialog.ShowDialog() == true)
+            {
+                if (!ViewData.Load(dialog.FileName, ref vd))
+                {
+                    MessageBox.Show($"File not loaded correctly");
+                    vd = new ViewData();
+                }
+                DataContext = vd;
+            }
+        }
+
+        private void MenuFile_Save(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dialog = new Microsoft.Win32.SaveFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                if (!vd.Save(dialog.FileName))
+                {
+                    MessageBox.Show($"File not saved correctly");
+                }
+            }
+        }
+
+        private void MenuEdit_AddTime_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                VMf vmf = new VMf();
+                if (function_choice.SelectedItem == button_Sin) vmf = VMf.vmdSin;
+                else if (function_choice.SelectedItem == button_Cos) vmf = VMf.vmdCos;
+                else if (function_choice.SelectedItem == null) { MessageBox.Show("Choose function"); return; }
+
+                int len = int.Parse(tbox_len.Text);
+                double[] seg = new double[2];
+                seg[0] = double.Parse(tbox_left.Text);
+                seg[1] = double.Parse(tbox_right.Text);
+                if (seg[0] > seg[1]) { MessageBox.Show("Left limit greater than the right limit"); return; }
+
+                VMGrid vmg = new VMGrid(vmf, len, seg);
+                vd.Change = vd.vmb.AddVMTime(vmg);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Incorrect input: " + ex.Message);
+            }
+        }
+
+        private void MenuEdit_AddAcc_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                VMf vmf = new VMf();
+                if (function_choice.SelectedItem == button_Sin) vmf = VMf.vmdSin;
+                else if (function_choice.SelectedItem == button_Cos) vmf = VMf.vmdCos;
+                else if (function_choice.SelectedItem == null) { MessageBox.Show("Choose function"); return; }
+
+                int len = int.Parse(tbox_len.Text);
+                double[] seg = new double[2];
+                seg[0] = double.Parse(tbox_left.Text);
+                seg[1] = double.Parse(tbox_right.Text);
+                if(seg[0] > seg[1]) { MessageBox.Show("Left limit greater than the right limit"); return; }
+
+                VMGrid vmg = new VMGrid(vmf, len, seg);
+                vd.Change = vd.vmb.AddVMAccuracy(vmg);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Incorrect input: "+ex.Message);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if(MessageBox.Show("Close window?", "", MessageBoxButton.YesNoCancel) == MessageBoxResult.No) e.Cancel = true;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (vd.Change == true)
+            {
+                if (MessageBox.Show("Data will be lost. Save Data?", "", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    Microsoft.Win32.SaveFileDialog dialog = new Microsoft.Win32.SaveFileDialog();
+                    if (dialog.ShowDialog() == true)
+                    {
+                        if (!vd.Save(dialog.FileName))
+                        {
+                            MessageBox.Show($"File not saved correctly");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp/ViewData.cs
@@ -0,0 +1,125 @@
+﻿using CLab;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public class ViewData : INotifyPropertyChanged
+    {
+        public VMBenchmark vmb = new VMBenchmark();
+
+        public VMBenchmark Vmb
+        {
+            get
+            {
+                return vmb;
+            }
+            set
+            {
+                vmb = value;
+                OnPropertyChanged(nameof(Vmb));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string property_name)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property_name));
+            }
+        }
+        public ViewData()
+        {
+            vmb = new VMBenchmark();
+        }
+
+        public void AddVMTime(VMGrid vmg)
+        {
+            Change = vmb.AddVMTime(vmg);
+        }
+
+        public void AddVMAccuracy(VMGrid vmg)
+        {
+            Change = vmb.AddVMAccuracy(vmg);
+        }
+
+        public bool Save(string filename)
+        {
+            FileStream file = null;
+            try
+            {
+                using (file = new FileStream(filename, FileMode.OpenOrCreate))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(ViewData));
+                    xmlSerializer.Serialize(file, this);
+                    Change = false;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Exception during file saving: {ex.Message}");
+                return false;
+            }
+            finally
+            {
+                if (file != null) file.Close();
+            }
+        }
+
+        public static bool Load(string filename, ref ViewData vd)
+        {
+            FileStream file = null;
+            try
+            {
+                using (file = new FileStream(filename, FileMode.Open))
+                {
+                    vd.vmb.accuracy.Clear();
+                    vd.vmb.time.Clear();
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(ViewData));
+                    vd = xmlSerializer.Deserialize(file) as ViewData;
+                    vd.Change = false;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                vd = new ViewData();
+                MessageBox.Show($"Exception during file loading: {ex.Message}");
+                return false;
+            }
+            finally
+            {
+                if (file != null) file.Close();
+            }
+        }
+
+        bool change = false;
+        public bool Change 
+        { 
+            get { return change; } 
+            set
+            {
+                change = value;
+                OnPropertyChanged(nameof(ifChanged));
+            }
+        }
+        public string ifChanged
+        {
+            get
+            { 
+                if (change) return "Collection was changed";
+                else return "Collection wasn't changed";
+            }
+        }
+    }
+}
